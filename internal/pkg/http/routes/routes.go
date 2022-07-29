5f05b16d6cef8fe3ce8c3b838a9bf892fba6191c
Vladimir Boltava <vaberof@gmail.com>
2022-07-29 13:47:54 +0700

    create api
    configure routes with api

--- internal/pkg/http/routes/routes.go
+++ internal/pkg/http/routes/routes.go
@@ -2,16 +2,16 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/vaberof/banking_app/internal/app/controllers"
+	"github.com/vaberof/banking_app/internal/api"
 )
 
 func Setup(app *fiber.App) {
-	app.Post("/signup", controllers.Register)
-	app.Post("/auth", controllers.Login)
-	app.Post("/logout", controllers.Logout)
-	app.Get("/balance", controllers.Balance)
-	app.Post("/account", controllers.CreateAccount)
-	app.Delete("/account", controllers.DeleteAccount)
-	app.Post("/transfer", controllers.Transfer)
-	app.Get("/history", controllers.Transactions)
+	app.Post("/signup", api.Signup)
+	app.Post("/auth", api.Login)
+	app.Post("/logout", api.Logout)
+	app.Get("/balance", api.GetBalance)
+	app.Post("/account", api.CreateNewAccount)
+	app.Delete("/account", api.DeleteAccount)
+	app.Post("/transfer", api.MakeTransfer)
+	app.Get("/history", api.GetTransactionsHistory)
 }
