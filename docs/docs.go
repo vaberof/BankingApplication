ee928b78bbc5e6c37ca592d5b50d6783a7314694
Vladimir Boltava <vaberof@gmail.com>
2022-08-12 18:43:57 +0700

    update swagger docs

--- docs/docs.go
+++ docs/docs.go
@@ -490,7 +490,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "mock-banking-application.herokuapp.com:80",
+	Host:             "mock-banking-application.herokuapp.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Banking App",
